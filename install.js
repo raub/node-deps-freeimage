7ab9770ff24a63314a0f1e9dbfddaca710be3003
Luis Blanco <luisblanco1337@gmail.com>
2021-05-06 22:01:17 +0300

    Update install tag

--- install.js
+++ install.js
@@ -4,6 +4,6 @@ const install = require('addon-tools-raub/install');
 
 
 const prefix = 'https://github.com/node-3d/deps-freeimage-raub/releases/download';
-const tag    = 'v3.0.0-pre';
+const tag    = 'v3.0.0';
 
 install(`${prefix}/${tag}`);
